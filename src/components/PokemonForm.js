abd9de5063d0b2d40f971bc1bef878a5c41ed21b
vokur13 <vokur13@gmail.com>
2022-10-16 17:39:26 +0300

    hooks added

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,43 +1,76 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ImSearch } from 'react-icons/im';
 import { toast } from 'react-toastify';
 
 const styles = { form: { marginBottom: 20 } };
 
-export class PokemonForm extends Component {
-  state = {
-    pokemonName: '',
-  };
+export function PokemonForm({ onSubmit }) {
+  const [pokemonName, setPokemonName] = useState('');
 
-  handleNameChange = event => {
-    this.setState({ pokemonName: event.currentTarget.value.toLowerCase() });
-  };
+  function handleNameChange(e) {
+    setPokemonName(e.currentTarget.value.toLowerCase());
+  }
 
-  handleSubmit = event => {
-    event.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    if (this.state.pokemonName.trim() === '') {
+    if (pokemonName.trim() === '') {
       return toast.error('Input Pokemon name please');
     }
 
-    this.props.onSubmit(this.state.pokemonName);
-    this.setState({ pokemonName: '' });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} style={styles.form}>
-        <input
-          type="text"
-          name="pokemonName"
-          value={this.state.pokemonName}
-          onChange={this.handleNameChange}
-        />
-        <button type="submit">
-          <ImSearch style={{ marginRight: 8 }} />
-          Найти
-        </button>
-      </form>
-    );
+    onSubmit(pokemonName);
+    setPokemonName('');
   }
+  return (
+    <form onSubmit={handleSubmit} style={styles.form}>
+      <input
+        type="text"
+        name="pokemonName"
+        value={pokemonName}
+        onChange={handleNameChange}
+      />
+      <button type="submit">
+        <ImSearch style={{ marginRight: 8 }} />
+        Найти
+      </button>
+    </form>
+  );
 }
+
+// export class protoPokemonForm extends Component {
+//   state = {
+//     pokemonName: '',
+//   };
+
+//   handleNameChange = event => {
+//     this.setState({ pokemonName: event.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+
+//     if (this.state.pokemonName.trim() === '') {
+//       return toast.error('Input Pokemon name please');
+//     }
+
+//     this.props.onSubmit(this.state.pokemonName);
+//     this.setState({ pokemonName: '' });
+//   };
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} style={styles.form}>
+//         <input
+//           type="text"
+//           name="pokemonName"
+//           value={this.state.pokemonName}
+//           onChange={this.handleNameChange}
+//         />
+//         <button type="submit">
+//           <ImSearch style={{ marginRight: 8 }} />
+//           Найти
+//         </button>
+//       </form>
+//     );
+//   }
+// }
--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PokemonDataView from './PokemonDataView';
 import PokemonErrorView from './PokemonErrorView';
 import PokemonPendingView from './PokemonPendingView';
@@ -11,36 +11,72 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-export class PokemonInfo extends Component {
-  state = { pokemon: null, error: null, status: Status.IDLE };
+export function PokemonInfo({ pokemonName }) {
+  const [pokemon, setPokemon] = useState(null);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.pokemonName !== this.props.pokemonName) {
-      this.setState({ status: Status.PENDING });
-      pokemonAPI
-        .fetchPokemon(this.props.pokemonName)
-        .then(pokemon => this.setState({ pokemon, status: Status.RESOLVED }))
-        .catch(error => {
-          this.setState({ error, status: Status.REJECTED });
-        });
+  useEffect(() => {
+    if (!pokemonName) {
+      return;
     }
+    setStatus(Status.PENDING);
+    pokemonAPI
+      .fetchPokemon(pokemonName)
+      .then(pokemon => {
+        setPokemon(pokemon);
+        setStatus(Status.RESOLVED);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      });
+  }, [pokemonName]);
+
+  if (status === Status.IDLE) {
+    return <div>Give us know PokemonName</div>;
   }
-
-  render() {
-    const { pokemon, error, status } = this.state;
-    const { pokemonName } = this.props;
-
-    if (status === 'idle') {
-      return <div>Give us know PokemonName</div>;
-    }
-    if (status === 'pending') {
-      return <PokemonPendingView pokemonName={pokemonName} />;
-    }
-    if (status === 'rejected') {
-      return <PokemonErrorView message={error.message} />;
-    }
-    if (status === 'resolved') {
-      return <PokemonDataView pokemon={pokemon} />;
-    }
+  if (status === Status.PENDING) {
+    return <PokemonPendingView pokemonName={pokemonName} />;
+  }
+  if (status === Status.REJECTED) {
+    return <PokemonErrorView message={error.message} />;
+  }
+  if (status === Status.RESOLVED) {
+    return <PokemonDataView pokemon={pokemon} />;
   }
 }
+
+// export class protoPokemonInfo extends Component {
+//   state = { pokemon: null, error: null, status: Status.IDLE };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevProps.pokemonName !== this.props.pokemonName) {
+//       this.setState({ status: Status.PENDING });
+//       pokemonAPI
+//         .fetchPokemon(this.props.pokemonName)
+//         .then(pokemon => this.setState({ pokemon, status: Status.RESOLVED }))
+//         .catch(error => {
+//           this.setState({ error, status: Status.REJECTED });
+//         });
+//     }
+//   }
+
+//   render() {
+//     const { pokemon, error, status } = this.state;
+//     const { pokemonName } = this.props;
+
+//     if (status === 'idle') {
+//       return <div>Give us know PokemonName</div>;
+//     }
+//     if (status === 'pending') {
+//       return <PokemonPendingView pokemonName={pokemonName} />;
+//     }
+//     if (status === 'rejected') {
+//       return <PokemonErrorView message={error.message} />;
+//     }
+//     if (status === 'resolved') {
+//       return <PokemonDataView pokemon={pokemon} />;
+//     }
+//   }
+// }
